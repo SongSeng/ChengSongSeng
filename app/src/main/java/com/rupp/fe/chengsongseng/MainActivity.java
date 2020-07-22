9f9dd40857707fc9c88b412d67e8a90bb4659f1f
SongSeng <45004925+SongSeng@users.noreply.github.com>
2020-07-22 16:05:12 +0700

    this is Fragment commit

--- app/src/main/java/com/rupp/fe/chengsongseng/MainActivity.java
+++ app/src/main/java/com/rupp/fe/chengsongseng/MainActivity.java
@@ -2,7 +2,11 @@ package com.rupp.fe.chengsongseng;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ChangeFragment(View view){
+        Fragment fragment;
+        if ( view == findViewById(R.id.button)){
+            fragment = new HomeFragment();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragLayout, fragment);
+            ft.commit();
+        }
+        if ( view == findViewById(R.id.button2)){
+            fragment = new CategoriesFragment();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragLayout, fragment);
+            ft.commit();
+        }
+        if ( view == findViewById(R.id.button3)){
+            fragment = new SearchFragment();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragLayout, fragment);
+            ft.commit();
+        }
+        if ( view == findViewById(R.id.button4)){
+            fragment = new AccountFragment();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragLayout, fragment);
+            ft.commit();
+        }
     }
 }
