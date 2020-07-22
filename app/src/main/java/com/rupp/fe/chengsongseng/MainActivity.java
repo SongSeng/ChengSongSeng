3bdd0fcac35439493620a805b3fdc5e96841afff
SongSeng <45004925+SongSeng@users.noreply.github.com>
2020-07-22 17:21:38 +0700

    Last Commit

--- app/src/main/java/com/rupp/fe/chengsongseng/MainActivity.java
+++ app/src/main/java/com/rupp/fe/chengsongseng/MainActivity.java
@@ -5,17 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<Product> listProducts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        
     }
 
+    private void loadRecyclerViewData(){
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+
+    }
     public void ChangeFragment(View view){
         Fragment fragment;
         if ( view == findViewById(R.id.button)){
